d509766377a6ef1400eb9f1a5e09962e45f25d6f
Lukewitt169 <benfrankmain@gmail.com>
2025-01-31 08:11:18 -0500

    Update password.js

--- public/password.js
+++ public/password.js
@@ -1,1 +1,14 @@
-
+document.getElementById('passwordForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const password = document.getElementById('accessPassword').value;
+    const response = await fetch('/authenticate', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ password })
+    });
+    if (response.ok) {
+        window.location.href = '/login.html';
+    } else {
+        alert('Incorrect password');
+    }
+});
